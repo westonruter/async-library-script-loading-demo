338c351232953a0821373f8de46229617f3aaa00
Glitch (glitch-blank-website) <none>
2023-04-28 01:02:04 +0000

    ⏱🎄 Checkpoint ./runtime.js:7427215/4434

--- runtime.js
+++ runtime.js
@@ -2,36 +2,57 @@
   const componentPromises = {};
 
   // Logging for demo.
-  const logElement = document.getElementById('log');
-  logElement.textContent = '';
+  const logElement = document.getElementById("log");
+  logElement.textContent = "";
   lib.log = (message) => {
     logElement.textContent += `${message}\n`;
   };
-  lib.log( 'Runtime loaded' );
+  lib.log("Runtime loaded");
 
   lib.uponComponentsLoaded = (...componentSlugs) => {
     const promises = [];
-    for ( const slug of componentSlugs ) {
-      if ( ! slug in componentPromises ) {
-        componentPromises[ slug ] = new Promise();
+    for (const slug of componentSlugs) {
+      if (!slug in componentPromises) {
+        componentPromises[slug] = new Promise();
       }
     }
     return promises;
   };
-  
-  const init = ( slug, component, deps  ) => {
-    // TODO: Wait for the deps to load.
-    component( lib );
+
+  const getComponentPromises = async (deps) => {
+    const promises = [];
+    for (const dep of deps) {
+      if (!componentPromises[dep]) {
+        let resolve, reject;
+        let promise = new Promise((res, rej) => {
+          [resolve, reject] = [res, rej];
+        });
+        componentPromises[dep] = { promise, resolve, reject };
+      }
+      promises.push(componentPromises[dep]);
+    }
+    return promises;
   };
-  
+
+  const init = async(slug, component, deps = []) => {
+    if (componentPromises[slug]) {
+      componentPromises[slug].resolve(component);
+    }
+
+    await Promise.all(getComponentPromises(deps));
+
+    // TODO: Wait for the deps to load.
+    component(lib);
+  };
+
   // Execute any components that have been previously registered.
-  while ( lib.length) {
-    const [ slug, component, deps ] = lib.shift();
-    init( slug, component, deps );
+  while (lib.length) {
+    const [slug, component, deps] = lib.shift();
+    init(slug, component, deps);
   }
-  
+
   // When any subsequent components are registered, execute immediately.
   lib.push = ([slug, component, deps]) => {
-    init( slug, component, deps );
+    init(slug, component, deps);
   };
-})(self.MyAsyncLib = self.MyAsyncLib || []);
+})((self.MyAsyncLib = self.MyAsyncLib || []));
